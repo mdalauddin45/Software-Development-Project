567ad425ec3597990e47c72d6f77d95f7f371e0c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-18 23:19:33 +0600

    array

---
+++ Module 5/arry.js
@@ -0,0 +1,8 @@
+let fruits = ['Apple', 'Banana', 'Orange'];
+let numbers = [1, 2, 3, 4, 5];
+let mixedArray = ['Hello', 123, true, { key: 'alu' }];
+fruits.push("dim");
+// numbers.pop();
+numbers.splice(1,3);
+// console.log(mixedArray[3].key);
+console.log(numbers)
