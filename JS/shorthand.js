f5c209540966db596093ffcdadff8633b1532d03
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-18 22:25:11 +0600

    first commit

---
+++ JS/shorthand.js
@@ -0,0 +1,12 @@
+var tomatoprice = 25;
+tomatoprice=30;
+
+var price1 = 31;
+var price2 = 10;
+price1=price1+price2;
+
+console.log(price2);
+console.log(tomatoprice);
+
+price1+=price2;
+console.log(price1);
---
+++ JS/variable.js
@@ -0,0 +1,17 @@
+var x=10;
+var y=10;
+console.log(x+y);
+
+//numaric
+var price = 11;
+var age = 12;
+var temperature = 13;
+
+//string 
+var person = "alauddin";
+var name = "alauddin";
+var email = "alauddin.com";
+
+//boolean
+var boolean = true;
+var isFullmark = false;
