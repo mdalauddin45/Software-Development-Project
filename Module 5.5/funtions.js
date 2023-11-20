71cef1ae13dff4a9ea0e06279cc4567f9ad61f60
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-20 22:24:40 +0600

    dom

---
+++ Module 5.5/funtions.js
@@ -0,0 +1,59 @@
+function sum(a,b){
+    console.log(a+b);
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+function mod(a,b){
+    return a%b;
+}
+
+function pow(a,b){
+    return Math.pow(a,b);
+}
+
+function sqrt(a){
+    return Math.sqrt(a);
+}
+
+function abs(a){
+    return Math.abs(a);
+}
+
+function sin(a){
+    return Math.sin(a);
+}
+
+function cos(a){
+    return Math.cos(a);
+}
+
+function tan(a){
+    return Math.tan(a);
+}
+
+function asin(a){
+    return Math.asin(a);
+}
+
+function acos(a){
+    return Math.acos(a);
+}
+
+function atan(a){
+    return Math.atan(a);
+}
+
+// ans in sum 
+// console.log(sum(1,3));
+sum(1,5);
