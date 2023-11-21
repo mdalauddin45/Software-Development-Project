e4ea4940b386fcf710643080d0dc41c7c2510fa6
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-21 22:24:40 +0600

    let var const

---
+++ Module 7/app.js
@@ -0,0 +1,25 @@
+
+var name = "Alauddin";
+
+let name2 = "slauddin";
+
+const name3 = "kalauddin";
+
+// function names(){
+//     if(true){
+//         var x = 4;
+//         let y = 10;
+//         const z = 12;
+//     }
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// console.log(names());
+
+name2 = "vlauddin";
+console.log(name2);
+
+name3 = "taltouddin";
+console.log(name3);
