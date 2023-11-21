3e5542357212cca94e97f3557561e8ae342a88bd
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-21 20:06:55 +0600

    app

--- Module 6/app.js
+++ Module 6/app.js
@@ -1,1 +1,8 @@
-alert("hello world!");
+var  h1 = document.getElementsByTagName("h1");
+console.log(h1);
+
+var title = document.getElementById("title").style.color = "red";
+console.log(title);
+
+var child = document.getElementsByClassName("child");
+console.log(child[0]);
