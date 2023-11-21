c191e0718da2d4574c180831dce48462888268e8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-21 22:46:12 +0600

    array function

--- Module 7/app.js
+++ Module 7/app.js
@@ -1,9 +1,9 @@
 
-var name = "Alauddin";
+// var name = "Alauddin";
 
-let name2 = "slauddin";
+// let name2 = "slauddin";
 
-const name3 = "kalauddin";
+// const name3 = "kalauddin";
 
 // function names(){
 //     if(true){
@@ -18,8 +18,19 @@ const name3 = "kalauddin";
 
 // console.log(names());
 
-name2 = "vlauddin";
+// name2 = "vlauddin"; 
+// console.log(name2);
+
+// name3 = "taltouddin";
+// console.log(name3);
+
+
+const name = `talauddin`;
+const name2 = `${name} hello`;
 console.log(name2);
 
-name3 = "taltouddin";
-console.log(name3);
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+console.log(...num);
+
+const test = ()=> 2*2;
+console.log(test());
