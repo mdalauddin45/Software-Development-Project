8179adc1492fb576f2f0e121d11bf3eb1c3d7f2b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-11-22 23:51:52 +0600

    done

--- Module 7.5/app.js
+++ Module 7.5/app.js
@@ -1,5 +1,30 @@
 const loadData=()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(res=>res.json())
-    .then(data=> console.log(data))
+    .then(data=> displyUser(data))
+    .catch(error => console.error('Error:', error));
 }
+const displyUser = (users)=>{
+    console.log(users);
+    const userContainer = document.getElementById('userCards');
+    users.forEach(user => {
+        const userCard = 
+        `
+        <div class="col-md-4 mb-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${user.name}</h5>
+              <p class="card-text">Username: ${user.username}</p>
+              <p class="card-text">Email: ${user.email}</p>
+              <p class="card-text">Phone: ${user.phone}</p>
+              <p class="card-text">Website: ${user.website}</p>
+              <p class="card-text">City: ${user.address.city}</p>
+            </div>
+          </div>
+        </div>
+        `
+        userContainer.innerHTML += userCard;
+    });
+}
+
+loadData();
